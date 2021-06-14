147f2418a718e6e2226e36327471d7d0ca037d43
rafi <rafi9933ulomar@gmail.com>
2021-06-14 13:49:00 +0600

    full program

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ void n3(void){ // taking 3 new lines
 	cout<<"\n\n\n\n";
 }
 
+void t5(void){ // taking 5 tabs
+	cout<<"\t\t\t\t\t";
+}
+
 
 //questions parts:
 
@@ -64,49 +68,67 @@ string ans_5(string a){
 	return r;
 }
 
+string report(string& q1, string& q2, string& q3, string& q4, string& q5){
+string r1,r2,r3,r4,r5;
+r1=q1; r2=q2; r3=q3; r4=q4; r5=q5;
 
+t5();cout<<"Personal Survey Details";
+n3();
+
+cout<<"Full Name : "<<r1<<" "<<r2<<"\n";
+cout<<"Age       : "<<r3<<"\n";
+cout<<"Profession: "<<r4<<"\n";
+cout<<"Country   : "<<r5<<"\n";
+
+return r1,r2,r3,r4,r5;
+}
 
 
 int main(){
 	welcome();
 	
-	string q1, q2, q3, q4, q5, q6, q7, q8;
+	string q1, q2, q3, q4, q5, q6, q7, q8, an1 ,an2, an3, an4, an5;
+
 
 
 
 	n3();
 	question_1();
 	getline(cin, q1);
-	ans_1(q1);
+	an1 = ans_1(q1);
 
 
 
 	n3();
 	question_2();
 	getline(cin, q2);
-	ans_2(q2);
+	an2 = ans_2(q2);
 	
 	
 
 	n3();
 	question_3();
 	getline(cin, q3);
-	ans_3(q3);
+	an3 = ans_3(q3);
 
 
 
 	n3();
 	question_4();
 	getline(cin, q4);
-	ans_4(q4);
+	an4 = ans_4(q4);
 
 
 	n3();
 	question_5();
 	getline(cin, q5);
-	ans_5(q5);
+	an5 = ans_5(q5);
 
 
+
+//showing report though reports function
+
+	report(an1, an2, an3, an4, an5);
 
 	return 0;
 }
