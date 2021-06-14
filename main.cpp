c0c6e7b4cc77401ab90a39c7735d5c8599616e69
rafi <rafi9933ulomar@gmail.com>
2021-06-14 13:18:57 +0600

    stracture

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 #include <string>
 using namespace std;
 
+
+//extra staff:
+
+void n3(void){ // taking 3 new lines
+	cout<<"\n\n\n\n";
+}
+
+
+//questions parts:
+
 void welcome(void){ // info about starting details
-	cout<<"Welcome to Pesonal Survey Program";	
+	cout<<"\t\t\t\t\tWelcome to Pesonal Survey Program";	
 }
 void question_1(void){ // asking first name
 	cout<<"What is your first name ? : ";
@@ -17,22 +27,84 @@ void question_3(void){ // asking recent age
 	cout<<"What is your actual age ? : ";
 }
 
-void question_4(void){
+void question_4(void){ // asking profession
 	cout<<"What is your Profession ? : ";
 }
 
-void question_5(void){
+void question_5(void){ // asking country
 	cout<<"Which country do you belong ? : ";
 }
 
 
-int main(){
-	question_1();
-	question_2();
-	question_3();
-	question_4();
-	question_5();
+// getting answars:
 
+
+string ans_1(string a){
+	string r = a ;
+	return r;
+}
+
+string ans_2(string a){
+	string r = a ;
+	return r;
+}
+
+string ans_3(string a){
+	string r = a ;
+	return r;
+}
+
+string ans_4(string a){
+	string r = a ;
+	return r;
+}
+
+string ans_5(string a){
+	string r = a ;
+	return r;
+}
+
+
+
+
+int main(){
+	welcome();
+	
+	string q1, q2, q3, q4, q5, q6, q7, q8;
+
+
+
+	n3();
+	question_1();
+	getline(cin, q1);
+	ans_1(q1);
+
+
+
+	n3();
+	question_2();
+	getline(cin, q2);
+	ans_2(q2);
+	
+	
+
+	n3();
+	question_3();
+	getline(cin, q3);
+	ans_3(q3);
+
+
+
+	n3();
+	question_4();
+	getline(cin, q4);
+	ans_4(q4);
+
+
+	n3();
+	question_5();
+	getline(cin, q5);
+	ans_5(q5);
 
 
 
